810631b34e3bc156c7cd9d95cbc9c2171c95a9fa
Fran López <franlp40@gmail.com>
2019-01-14 19:03:37 +0100

    Library not found in AF

--- Correlation_in_AF/test_AF.py
+++ Correlation_in_AF/test_AF.py
@@ -10,11 +10,11 @@ import numpy as np
 
 import time
 
-from correlation_lib import compute_correlation
+from correlationAF_lib import compute_correlation
 
-events = np.load('events_eruption_v8.npy',fix_imports=True, encoding='latin1')
+#events = np.load('events_eruption_v8.npy',fix_imports=True, encoding='latin1')
 
-#events=np.ones((128, 1500),dtype=np.float64)
+events=np.ones((10, 100),dtype=np.float64)
 
 
 tiempos = np.zeros(1)
@@ -22,13 +22,14 @@ tiempos = np.zeros(1)
 for i in range(1):
 	start = time.time()
 
-	xcm_pos, xclags_pos, xcm_neg, xclags_neg = compute_correlation(events,250)
+	padded_events, xcm_pos, xclags_pos, xcm_neg, xclags_neg = compute_correlation(events,1)
 
 	diff_time = time.time() - start
 	print("Execution finished in: " + str(diff_time))
 	tiempos[i] = diff_time
-    
 print("Max: " + str(tiempos.max()) + " Mean: "+ str(tiempos.mean()) + " Min: "+str(tiempos.min()))
+
+np.savetxt('events.txt', padded_events, delimiter='\t', fmt='%6.3f')
 
 np.savetxt('xcm_v1_neg.txt',xcm_neg, delimiter='\t', fmt='%6.3f')
 
