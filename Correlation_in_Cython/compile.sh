1c9ad07177350bdc26e63f2aee9161b4ab0420a8
Rafael Asenjo <asenjo@ac.uma.es>
2019-05-13 14:11:31 +0200

    Better way to make prange work on mac. There are performance issues due to GIL, though

--- Correlation_in_Cython/compile.sh
+++ Correlation_in_Cython/compile.sh
@@ -2,4 +2,6 @@ python setup.py build_ext --inplace
 rm -rf build
 #on MAC instal gcc-9.1 con homebrew
 
-CC=g++-9 python par_setup.py build_ext --inplace
+# Set DYLIB_LIBRARY_PATH=/usr/local/Cellar/gcc/8.2.0/lib/gcc/8 python test.py
+# CC=g++-9 python par_setup.py build_ext --inplace
+python par_setup.py build_ext --inplace
--- Correlation_in_Cython/par_setup.py
+++ Correlation_in_Cython/par_setup.py
@@ -2,15 +2,33 @@ from distutils.core import setup, Extension
 from Cython.Build import cythonize
 import numpy
 
+from distutils import sysconfig
 
-# Or, if you use cythonize() to make the ext_modules list,
-# include_dirs can be passed to setup()
+# me make damn sure, that disutils does not mess with our
+# build process
+
+sysconfig.get_config_vars()['CFLAGS'] = '-Wno-unused-result -Wsign-compare -Wunreachable-code -DNDEBUG -g -fwrapv -O3 -Wall -Wstrict-prototypes -I/anaconda3/include -I/anaconda3/include/python3.6m'
+sysconfig.get_config_vars()['OPT'] = ''
+sysconfig.get_config_vars()['PY_CFLAGS'] = ''
+sysconfig.get_config_vars()['PY_CORE_CFLAGS'] = ''
+sysconfig.get_config_vars()['CC'] = 'gcc-9'
+sysconfig.get_config_vars()['CXX'] = 'g++-9'
+sysconfig.get_config_vars()['BASECFLAGS'] = ''
+sysconfig.get_config_vars()['CCSHARED'] = '-fPIC'
+sysconfig.get_config_vars()['LDSHARED'] = 'gcc-9 -bundle -undefined dynamic_lookup -L/anaconda3/lib -arch x86_64'
+sysconfig.get_config_vars()['CPP'] = ''
+sysconfig.get_config_vars()['CPPFLAGS'] = ''
+sysconfig.get_config_vars()['BLDSHARED'] = ''
+sysconfig.get_config_vars()['CONFIGURE_LDFLAGS'] = ''
+sysconfig.get_config_vars()['LDFLAGS'] = ''
+sysconfig.get_config_vars()['PY_LDFLAGS'] = ''
+
 ext = Extension(
     name="par_correlate_cython_wrapper",
     sources=["par_correlate_cython_wrapper.pyx"],
     include_dirs=[numpy.get_include()],
     extra_compile_args=['-O3', '-fopenmp'],
-    extra_link_args=['-fopenmp']
+    extra_link_args=['-fopenmp','-Wl,-rpath,/usr/local/Cellar/gcc/9.1.0/lib/gcc/9']
 )
 
 setup(ext_modules=cythonize(ext))
